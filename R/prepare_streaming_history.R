7a1b59635fac70082e17429d452c1fabb339c6d7
mabuszka <magda@buszka.eu>
2020-05-06 21:40:21 +0200

    adds basic package files and one function

---
+++ R/prepare_streaming_history.R
@@ -0,0 +1,28 @@
+#' Prepares the streaming history data table
+#'
+#' Prepares the data table containig streaming history for futher oprations.
+#'  Changes the names of the base columns to: end_time, artist_name, track_name, s_played. Changes the end_time 
+#'  column from charcter to POSTIX date, s_played to a duration in seconds. Adds new columns: 
+#'  start_time - estimated start time calculated from end_time and s_played, skipped - true if the track was 
+#'  played for less than 10s, weekday - weekdays derived from start_time date.  \cr
+#'  ! It changes the orginal data table and not copies it and returns the changed copy ! 
+#' 
+#'
+#' @param streaming_history data.table containing streaming history
+#'#' 
+#'
+#' @export
+#' 
+#' @import data.table
+#' @importFrom lubridate dmilliseconds wday ymd_hm duration 
+#' 
+
+prepare_streaming_history <- function(streaming_history){
+  setnames(streaming_history,c("end_time", "artist_name", "track_name", "s_played"))
+  streaming_history[,`:=`(end_time = ymd_hm(end_time), s_played = dmilliseconds((s_played)))
+                    ][,`:=`(start_time = end_time - s_played,
+                            skipped = (s_played < duration(10, "seconds")))
+                      ][,weekday := lubridate::wday(start_time, label = TRUE)]
+  streaming_history
+}
+
