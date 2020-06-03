e6a8556031062b389bdbaacfdb0508f2224422ed
mabuszka <magda@buszka.eu>
2020-06-03 02:45:06 +0200

    adds and edits streaming history functions
    Added :
    -a new function combining number_of_tracks_hour/weekday into one function, but without period filtering within
    - a function for period filtering streaming history
    - helper function for capitalizing words
    Also some minor changes in rest of the functions.

--- R/number_of_tracks_by_hour.R
+++ R/number_of_tracks_by_hour.R
@@ -4,7 +4,7 @@
 #' Either collectively or separetely for each day of the week. Can be set to inculde songs that were counted as skipped.
 #' 
 #' 
-#' @param streaming_history A data.table containing streaming history, after 'prepare_streaming_history' was used on it.
+#' @param filtered A data.table containing streaming history, after 'prepare_streaming_history' was used on it.
 #' @param start_date A POSIXt indicating start of the period of time.
 #' @param end_date A POSIXt indicating end of the period of time.
 #' @param by_weekday A logic scalar indicating whether to show data for each weekday separately on the graph.
@@ -19,13 +19,10 @@
 
 
 
-number_of_tracks_by_hour <- function(streaming_history, start_date, end_date, 
+number_of_tracks_by_hour <- function(filtered, start_date, end_date, 
                                      by_weekday = FALSE, show_skipped = FALSE){
-  if (show_skipped){
-    filtered <- streaming_history[start_date <= start_time & end_date >= end_time,]
-  }
-  else{
-    filtered <- streaming_history[start_date <= start_time & end_date >= end_time & skipped == FALSE, ]
+  if (!show_skipped){
+    filtered <- filtered[skipped == FALSE, ]
   }
   vis <- ggplot(filtered, aes(x = hour(start_time)))+
     geom_bar()+
--- R/number_of_tracks_by_weekday.R
+++ R/number_of_tracks_by_weekday.R
@@ -4,12 +4,11 @@
 #' Can be set to inculde songs that were counted as skipped 
 #' 
 #' 
-#' @param streaming_history A data.table containing streaming history, after 'prepare_streaming_history' was used on it.
-#' @param start_date A POSIXt indicating start of the period of time.
-#' @param end_date A POSIXt indicating end of the period of time.
-#' @param show_skipped A logical scalar indicating whether to include tracks that were skipped in counting.
+#' @param filtered A data.table containing streaming history, after 'prepare_streaming_history' was used on it.
 #' @param as_percentage A logical scalar. If TRUE (dafault) bars show percentage of of all tracks played for each weekday.
-#' 
+#' @param period A character vector indicating for which periods to split the time for counting tracks played 
+#' @param include_skipped A logical scalar indicating whether to include tracks that were skipped
+
 #' @export
 #' 
 #' @import data.table
@@ -17,26 +16,23 @@
 #' 
 
 
-number_of_tracks_by_weekday <- function(streaming_history, start_date, end_date, show_skipped = FALSE, as_percentage = TRUE){
-    if (show_skipped){
-      filtered <- streaming_history[start_date <= start_time & end_date >= end_time,]
-    }
-    else{
-      filtered <- streaming_history[start_date <= start_time & end_date >= end_time & skipped == FALSE, ]
-    }
+number_of_tracks_by_period <- function(filtered, as_percentage = TRUE, include_skipped = FALSE){
+  if (!inculed_skipped){filtered <- filtered[skipped == FALSE,]}
   vis <- ggplot(filtered, aes(x = weekday))
   
   if (as_percentage){
     vis <- vis+
       geom_bar(aes(y = (..count..)/sum(..count..)))+
-      xlab("Weekday")+
+      xlab(toupper(period))+
       ylab("Percentage of all tracks played")
   }
   else{
     vis <- vis +
       geom_bar()+
-      xlab("Weekday")+
+      xlab(toupper(period))+
       ylab("Tracks played")
   }
   vis
 }
+
+
